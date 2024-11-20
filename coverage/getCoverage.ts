fcbbbd946e2b9a7b09506f0d93ac139e53a40a43
Cole J. Calamos <cole@calamos.family>
2024-11-20 16:28:06 -0600

    Refactoring testing report

--- coverage/getCoverage.ts
+++ coverage/getCoverage.ts
@@ -1,19 +1,23 @@
 import { parse } from "jsr:@std/jsonc";
 
 async function runTests(type: string, coverageDir: string) {
-  console.log(`Running ${type} tests and saving coverage to ${coverageDir}`);
-
   let testSuite: string;
+  let reportType: string;
 
   if (type === "all") {
     testSuite = "all-tests";
+    reportType = "All";
   } else if (type === "unit") {
     testSuite = "unit-tests";
+    reportType = "Unit";
   } else if (type === "integration") {
     testSuite = "integration-tests";
+    reportType = "Integration";
   } else {
     throw new Error(`Invalid test type: ${type}`);
   }
+
+  console.log(`\x1b[1m\x1b[33m[${reportType} Tests Report]:\x1b[0m Running ${type} tests and saving coverage to ${coverageDir}`);
 
   const cmd = new Deno.Command("deno", {
     args: [
@@ -27,6 +31,21 @@ async function runTests(type: string, coverageDir: string) {
   if (!success) {
     throw new Error(`${type} tests failed`);
   }
+
+  const coverageGenerator = new Deno.Command("deno", {
+    args: ["coverage", coverageDir, "--html"],
+  });
+
+  const { success: coverageSuccess } = await coverageGenerator.output();
+  if (!coverageSuccess) {
+    throw new Error(`Failed to generate coverage report`);
+  }
+
+  const fullPath = `file://${Deno.cwd()}/${coverageDir}/html/index.html`;
+
+  console.log(`\x1b[1m\x1b[33m[${reportType} Tests Report]:\x1b[0m \x1b[4m\x1b[34m\x1b]8;;${fullPath}\x1b\\View Coverage Report\x1b]8;;\x1b\\\x1b[0m`);
+
+  console.log(`\x1b[1m\x1b[33m[${reportType} Tests Report]:\x1b[0m \x1b[32mDone!\x1b[0m`);
 }
 
 export async function getFolder(args: string[]): Promise<void> {
