db8fda2969a2cd3413b09fa762dda05fd960ad35
Cole J. Calamos <cole@calamos.family>
2024-11-20 16:11:22 -0600

    Refactor

--- src/loader/loader.ts
+++ src/loader/loader.ts
@@ -6,51 +6,12 @@ interface AliasMapKey {
 }
 
 class Loader {
-  // #islandRegistry: Map<ComponentType, Island>;
   #aliasMap: Map<string, AliasMapKey>;
 
   constructor() {
     // this.#islandRegistry = new Map();
     this.#aliasMap = new Map();
   }
-
-  public addIsland(
-    alias: string,
-    baseUrl: AliasMapKey["baseUrl"],
-    componentPath: AliasMapKey["componentPath"],
-  ) {
-    this.#aliasMap.set(alias, {
-      baseUrl,
-      componentPath,
-    });
-  }
-
-  static IS_BROWSER(): boolean {
-    return typeof window !== "undefined";
-  }
-
-  // public async loadIsland(alias: string): Promise<() => unknown> {
-  //   const ServerIsland = await import(alias);
-
-  //   const Loading = () => h("div", {}, "Loading...");
-
-  //   return () => {
-  //     if (!IS_BROWSER) {
-  //       return h(Fragment, {}, ServerIsland.default());
-  //     }
-  //     const ClientIsland = lazy(() => import(alias));
-
-  //     return h(
-  //       Fragment,
-  //       {},
-  //       h(
-  //         Suspense,
-  //         { fallback: h(Loading, {}) },
-  //         h(ClientIsland, { message: "Hello from Lazy Component!" }),
-  //       ),
-  //     );
-  //   };
-  // }
 }
 
 export { Loader };
--- src/mod.ts
+++ src/mod.ts
@@ -1,1 +1,8 @@
+export * from "./config/mod.ts";
 export * from "./app/mod.ts";
+export * from "./builder/mod.ts";
+export * from "./handler/mod.ts";
+export * from "./loader/mod.ts";
+export * from "./router/mod.ts";
+
+export const IS_BROWSER = (): boolean => typeof window !== "undefined";
