d2e6a24b4865e3c228074c84b40121b51cce7ea8
Cole J. Calamos <cole@calamos.family>
2024-12-27 14:57:16 -0600

    Updated Builders definition

--- kintsugi/src/builder/builder.ts
+++ kintsugi/src/builder/builder.ts
@@ -1,11 +1,7 @@
 // @ts-self-types="./builder.d.ts"
 
 /**
- * The Builder class is responsible for building the modules from the source code.
- * It will take in the source code and return code that can be understood by the requested environment, whether that be Deno or a browser.
- * Builder will take an import map and use that to resolve the imports, this will allow for the use of aliases in the source code.
- *
- * What this needs to do will be determined by the loader class. As loader needs to be able to specify what should be loaded and builder will decide how to bundle.
+ * This Builder class is to be inherited by other builders. This will provide Kintsugi with the ability to bundle the code, independent of the bundler.
  */
 export class Builder {
   constructor() {}
